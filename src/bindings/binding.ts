83ccd275e398081d60cc06bc200966a7187cf072
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2021-04-04 10:17:16 +0300

    Use TS features in class Binding<T>

--- src/bindings/binding.ts
+++ src/bindings/binding.ts
@@ -4,62 +4,56 @@ import { id } from "../utils/id";
 
 class Binding<T> implements interfaces.Binding<T> {
 
-    public id: number;
+    public id: number = id();
     public moduleId: string;
 
     // Determines weather the bindings has been already activated
     // The activation action takes place when an instance is resolved
     // If the scope is singleton it only happens once
-    public activated: boolean;
+    public activated: boolean = false;
 
     // A runtime identifier because at runtime we don't have interfaces
     public serviceIdentifier: interfaces.ServiceIdentifier<T>;
 
     // The constructor of a class which must implement T
-    public implementationType: interfaces.Newable<T> | null;
+    public implementationType: interfaces.Newable<T> | null = null;
 
     // Cache used to allow singleton scope and BindingType.ConstantValue bindings
-    public cache: T | null;
+    public cache: T | null = null;
 
     // Cache used to allow BindingType.DynamicValue bindings
-    public dynamicValue: ((context: interfaces.Context) => T) | null;
+    public dynamicValue: ((context: interfaces.Context) => T) | null = null;
 
     // The scope mode to be used
-    public scope: interfaces.BindingScope;
+    public scope: interfaces.BindingScope = BindingScopeEnum.Transient;
 
     // The kind of binding
-    public type: interfaces.BindingType;
+    public type: interfaces.BindingType = BindingTypeEnum.Invalid;
 
     // A factory method used in BindingType.Factory bindings
-    public factory: interfaces.FactoryCreator<T> | null;
+    public factory: interfaces.FactoryCreator<T> | null = null;
 
     // An async factory method used in BindingType.Provider bindings
-    public provider: interfaces.ProviderCreator<T> | null;
+    public provider: interfaces.ProviderCreator<T> | null = null;
 
     // A constraint used to limit the contexts in which this binding is applicable
-    public constraint: (request: interfaces.Request) => boolean;
+    public constraint: (request: interfaces.Request) => boolean = (request: interfaces.Request) => true;
 
     // On activation handler (invoked just before an instance is added to cache and injected)
-    public onActivation: ((context: interfaces.Context, injectable: T) => T) | null;
+    public onActivation: ((context: interfaces.Context, injectable: T) => T) | null = null;
 
-    public constructor(serviceIdentifier: interfaces.ServiceIdentifier<T>, scope: interfaces.BindingScope) {
-        this.id = id();
-        this.activated = false;
+    public constructor(serviceIdentifier: interfaces.ServiceIdentifier<T>, scope?: interfaces.BindingScope) {
         this.serviceIdentifier = serviceIdentifier;
-        this.scope = scope;
-        this.type = BindingTypeEnum.Invalid;
-        this.constraint = (request: interfaces.Request) => true;
-        this.implementationType = null;
-        this.cache = null;
-        this.factory = null;
-        this.provider = null;
-        this.onActivation = null;
-        this.dynamicValue = null;
+        if (scope) this.scope = scope;
+    }
+
+    static of<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>, scope: interfaces.BindingScope) {
+        return new Binding<T>(serviceIdentifier, scope)
     }
 
     public clone(): interfaces.Binding<T> {
         const clone = new Binding(this.serviceIdentifier, this.scope);
-        clone.activated = (clone.scope === BindingScopeEnum.Singleton) ? this.activated : false;
+        clone.activated = (this.scope === BindingScopeEnum.Singleton) ? this.activated : false;
         clone.implementationType = this.implementationType;
         clone.dynamicValue = this.dynamicValue;
         clone.scope = this.scope;
