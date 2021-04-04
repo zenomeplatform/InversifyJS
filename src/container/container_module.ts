209cd706845b3f87940a67aec39a95a0eac658d2
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2021-04-04 12:16:42 +0300

    Container module class simlify

--- src/container/container_module.ts
+++ src/container/container_module.ts
@@ -2,23 +2,15 @@ import { interfaces } from "../interfaces/interfaces";
 import { id } from "../utils/id";
 
 export class ContainerModule implements interfaces.ContainerModule {
-
     public id: number|string = id();
-    public registry: interfaces.ContainerModuleCallBack;
-
-    public constructor(registry: interfaces.ContainerModuleCallBack) {
-        this.registry = registry;
-    }
-
+    constructor(
+        public registry: interfaces.ContainerModuleCallBack
+    ) {}
 }
 
 export class AsyncContainerModule implements interfaces.AsyncContainerModule {
-
     public id: number|string = id();
-    public registry: interfaces.AsyncContainerModuleCallBack;
-
-    public constructor(registry: interfaces.AsyncContainerModuleCallBack) {
-        this.registry = registry;
-    }
-
+    constructor(
+        public registry: interfaces.AsyncContainerModuleCallBack
+    ) {}
 }
